6a8e123c11eeb14561f2b9dd42e5a3fc88bc3357
Christian Bodart <chris@bodar.com>
2013-07-20 18:41:25 +0100

    now monitoring frame events

--- www/js/sync.js
+++ www/js/sync.js
@@ -17,22 +17,27 @@
 			this.buildFrameCells();
 		},
 		bindEvents: function () {
-			this.formSubmitHandlerBinding = this.formSubmitHandler.bind(this)
-			this.formElement.addEventListener("submit", this.formSubmitHandlerBinding);
-			this.checkboxChangeHandlerBinding = this.checkboxChangeHandler.bind(this);
-			this.formElement.addEventListener("change",this.checkboxChangeHandlerBinding,true);
+			this.formSubmitListenerBinding = this.formSubmitListener.bind(this)
+			this.formElement.addEventListener("submit", this.formSubmitListenerBinding);
+			this.checkboxChangeListenerBinding = this.checkboxChangeListener.bind(this);
+			this.formElement.addEventListener("change",this.checkboxChangeListenerBinding,true);
+			this.loadListenerBinding = this.loadListener.bind(this);
+			document.addEventListener("load",this.loadListenerBinding,true)
 		},
-		formSubmitHandlerBinding: null,
-		formSubmitHandler: function (event) {
+		formSubmitListenerBinding: null,
+		formSubmitListener: function (event) {
 			event.preventDefault()
 		},
-		checkboxChangeHandlerBinding: null,
-		checkboxChangeHandler: function (event) {
+		checkboxChangeListenerBinding: null,
+		checkboxChangeListener: function (event) {
 			if (event.target.checked) {
 				this.activateFrame(event.target.value);
 			} else {
 				this.deactivateFrame(event.target.value)
 			}
+		},
+		loadListener: function (event) {
+			new this.IFrameEventListener(event.target,this)
 		},
 		buildFrameCells: function () {
 			[].forEach.apply(
@@ -69,6 +74,39 @@
 		deactivateFrame: function (width) {
 			this.frameCells[width].classList.remove("activated");
 			this.activatedFrames[width] = this.frameCells[width].querySelector("iframe");
+		},
+		handleFrameEvent: function (iframe,event) {
+			console.log("%O",iframe);
+			console.log("%O",event);
 		}
 	}
 );
+
+(function (IFrameEventListener,prototype) {
+	window.Sync.prototype.IFrameEventListener = IFrameEventListener;
+	for (var i in prototype) {
+		IFrameEventListener.prototype[i] = prototype[i];
+	}
+})(
+	function IFrameEventListener(iframe,sync) {
+		this.iframe = iframe;
+		this.document = iframe.contentDocument;
+		this.sync = sync;
+		this.bindEvents();
+	},
+	{
+		frameEvents: ["click"],
+		bindEvents: function () {
+			this.frameEvents.forEach(
+				function (eventName) {
+					this.document.addEventListener(eventName,this,true)
+				},
+				this
+			);
+		},
+		handleEvent: function (event) {
+			this.sync.handleFrameEvent(this.iframe,event)
+		}
+	}
+
+)
