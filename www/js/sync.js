47e2c550458b7d957c21a51e6175dccb396da5f8
Christian Bodart <chris@bodar.com>
2013-07-21 20:39:45 +0100

    adding autocomplete on the toolba

--- www/js/sync.js
+++ www/js/sync.js
@@ -10,11 +10,15 @@
 	},
 	{
 		iframes: null,
+		activatedFrames: null,
+		historyId: "url",
 		build: function sync_build(formElement,outputElement) {
 			this.formElement = formElement;
 			this.outputElement = outputElement;
 			this.bindEvents();
 			this.buildFrameCells();
+			this.historyManager = new this.HistoryManager(this.historyId)
+			this.historyManager.build()
 		},
 		bindEvents: function () {
 			this.formSubmitListenerBinding = this.formSubmitListener.bind(this)
@@ -27,9 +31,14 @@
 		formSubmitListenerBinding: null,
 		formSubmitListener: function (event) {
 			var url = event.target.querySelector("#url").value;
-			for (var i in this.activatedFrames) {
-				this.activatedFrames[i].src = url;
+			if (this.historyManager.validateUrl(url)) {
+				for (var i in this.activatedFrames) {
+					this.activatedFrames[i].src = url;
+				}
+			} else {
+				alert("Please enter a valid url. \nNB: the url's must be on the same domain as this page")
 			}
+
 			event.preventDefault();
 		},
 		checkboxChangeListenerBinding: null,
@@ -109,7 +118,7 @@
 		this.bindEvents();
 	},
 	{
-		frameEvents: ["click","input","change"],
+		frameEvents: ["click","input","change","keydown","keyup","keypress"],
 		bindEvents: function () {
 			this.frameEvents.forEach(
 				function (eventName) {
@@ -135,9 +144,9 @@
 	},
 	{
 		duplicateEvent:function (event,targetFrame,selector) {
-			var target = this.getTarget(event,targetFrame,selector);
-			this.syncElementStates(event.target,target)
+			var target = this.getTarget(event,targetFrame,selector);	
 			this.createLocalEvent(event,target);
+			this.syncElementStates(event.target,target)
 		},
 		getTarget: function (event,targetFrame,selector) {
 			var possibleTargets,
@@ -158,38 +167,39 @@
 				if (targets.length == 1) {
 					target = targets[0];
 				} else {
-					throw new Error("unable to decide which element to target")
+					SafeError("unable to decide which element to target")
 				}
 			}
 			return target;
 		},
 		syncElementStates: function (sourceElement, targetElement) {
-			if (sourceElement.value) {
+			if ("value" in sourceElement) {
 				targetElement.value = sourceElement.value
 			}
 
 		},
 		compareElement: function (element1,element2) {
 			if (element1.textContent == element2.textContent) {
-				return true
+				return true;
 			}
-			return false
+			return false;
 		},
 		createLocalEvent: function (event,newTarget) {
+			if (newTarget) {
 			var localWindow = newTarget.ownerDocument? newTarget.ownerDocument.defaultView : newTarget.defaultView,
 				newEvent = this.eventFactories[this.getEventConstructor(event)](event,newTarget);
 			newTarget.dispatchEvent(newEvent);
+			}
 		},
 		getEventConstructor: function (event) {
 			return event.constructor.toString().match(/^function\s*([^(]*)\s*\(/)[1];
 		},
 		eventFactories: {
 			MouseEvent: function (srcEvent,target) {
-				var event = new MouseEvent(srcEvent.type);
-				event.initMouseEvent(
+				var event = new MouseEvent(
 					srcEvent.type, 
-                    srcEvent.canBubbleArg, 
-                    srcEvent.cancelableArg, 
+                    srcEvent.canBubble, 
+                    srcEvent.cancelable, 
                     target.ownerDocument? target.ownerDocument.defaultView : target.defaultView, 
                     srcEvent.detail, 
                     srcEvent.screenX, 
@@ -200,14 +210,37 @@
                     srcEvent.altKey, 
                     srcEvent.shiftKey, 
                     srcEvent.metaKey, 
-                    srcEvent.buttonArg, 
+                    srcEvent.button, 
                     null//srcEvent.relatedTargetArg
                 );
                 event.mocked = true;
                 return event;
 			},
 			Event: function (srcEvent,target) {
-				return new Event(srcEvent)
+				var event = new Event(
+					srcEvent.type,
+					srcEvent.bubbles,
+					srcEvent.cancelable
+				);
+				event.mocked = true;
+				return event
+			},
+			KeyboardEvent: function (srcEvent,target) {
+				var event = new KeyboardEvent(
+					srcEvent.type,
+					srcEvent.bubbles,
+					srcEvent.cancelable,
+					target.ownerDocument? target.ownerDocument.defaultView : target.defaultView,
+					srcEvent.char,
+					srcEvent.key,
+					srcEvent.location,
+					srcEvent.modifiers,
+					srcEvent.repeat,
+					srcEvent.locale
+				)
+
+				event.mocked = true
+				return event
 			}
 		}
 
@@ -297,7 +330,6 @@
 			requests.loaded = true;
 		},
 		mapRequestProperties: function (request) {
-			//console.log(request)
 			Object.keys(request).forEach(
 				function(key) {
 					var type = typeof request[key]
@@ -309,4 +341,76 @@
 		}
 	}
 	
-})()
+})();
+
+(function (Constructor,prototype) {
+	for (var i in prototype) {
+		Constructor.prototype[i] = prototype[i]
+	}
+	window.Sync.prototype.HistoryManager = Constructor;
+
+})(
+	function HistoryManager(inputId) {
+		this.inputId = inputId;
+		this.sessionHistory = [];
+	},
+	{
+		inputId: "",
+		datalist: null,
+	build: function () {
+		this.storageData = JSON.parse(localStorage.getItem(this.inputId));
+		if (!this.storageData) {
+			this.storageData = [];
+		}
+		this.datalist = this.createDataList();
+	},
+	createDataList: function () {
+		var datalist = document.createElement("datalist"),
+			input = document.getElementById(this.inputId);
+		datalist.id = this.inputId + "DataList";
+		this.storageData.forEach(
+			function (item) {
+				datalist.appendChild(this.createOption(item))
+			},
+			this
+		);
+		input.parentElement.appendChild(datalist)
+		input.setAttribute("list",datalist.id);
+		return datalist
+	},
+	createOption: function (value) {
+		var option = document.createElement("option")
+		option.value = value;
+		return option
+	},
+	validateUrl: function (url) {
+		var request = new XMLHttpRequest();
+		request.open("HEAD",url,false);
+		try {
+		request.send()
+		}
+		catch (e) {
+			SafeError(e)
+			return false;
+		}
+		if (request.status!=404) {
+			this.storeUrl(url);
+			return true;
+		} else {
+			return false;
+		}
+	},
+	storeUrl: function (url) {
+		if (this.storageData.indexOf(url) == -1) {
+			this.storageData.push(url);
+			localStorage.setItem(this.inputId,JSON.stringify(this.storageData))
+			this.datalist.appendChild(this.createOption(url));
+		}
+	}
+});
+
+function SafeError(error) {
+	setTimeout(function () {
+		throw error
+	})
+}
